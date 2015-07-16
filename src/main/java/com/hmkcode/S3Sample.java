4c7da0454028714f222c15894ae7798759daa379
julian1 <julian.atkinson71@gmail.com>
2015-07-17 08:22:57 +1000

    S3 browser simplify creation

--- src/main/java/com/hmkcode/S3Sample.java
+++ src/main/java/com/hmkcode/S3Sample.java
@@ -1,32 +1,10 @@
-/*
- * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+
 
 package com.hmkcode;
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.UUID;
 import java.util.List;
-
+import java.io.IOException;
 
 
 import com.amazonaws.AmazonClientException;
@@ -45,18 +23,54 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
+import com.amazonaws.auth.profile.ProfilesConfigFile;
+
+
 class S3Browser {
 
     private final AmazonS3 s3; 
     private final String bucketName; 
+
 
     public S3Browser( AmazonS3 s3, String bucketName ) {
         this.s3 = s3; 
         this.bucketName = bucketName;
     } 
 
-    List<String> get( String path )
+    public S3Browser( String credentialsPath, String profileName, String bucketName ) {
+ 
+        AWSCredentials credentials = null;
+        try {
+ 
+            ProfilesConfigFile config = new ProfilesConfigFile( credentialsPath );
+            ProfileCredentialsProvider provider = new ProfileCredentialsProvider(config, profileName); 
+            credentials = provider.getCredentials();
+
+        } catch (Exception e) {
+
+            System.out.println("JA couldn't load credentials " + e.getMessage() );
+
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+
+        System.out.println("authenticating");
+
+        this.s3 = new AmazonS3Client(credentials);
+        this.bucketName = bucketName;
+
+        System.out.println("done authenticating");
+    }
+
+    List<String> getDirs( String path )   // change name to getDirs() or get getChildDirs() or getVirtualDirs() etc.
     {
+        while( path.charAt(0) == '/') {
+            path = path.substring(1);
+        }
+
         ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
             .withBucketName(bucketName)
            // .withPrefix("home/meteo/")   // 
@@ -68,8 +82,6 @@ class S3Browser {
 
         return objectListing.getCommonPrefixes();
     }
-
-
 }
 
 
@@ -77,47 +89,24 @@ public class S3Sample {
 
     static void recurse( S3Browser browser, String path )
     {
-        for (String key : browser.get(path)) {
+        for (String key : browser.getDirs(path)) {
             System.out.println(" - " + key );
             recurse( browser, key );
         }
     }
 
-
     public static void main(String[] args) throws IOException {
 
         System.out.println("main()");
 
-        /*
-         * The ProfileCredentialsProvider will return your [default]
-         * credential profile by reading from the credentials file located at
-         * (~/.aws/credentials).
-         */
-        AWSCredentials credentials = null;
-        try {
-            credentials = new ProfileCredentialsProvider().getCredentials();
-        } catch (Exception e) {
-            throw new AmazonClientException(
-                    "Cannot load the credentials from the credential profiles file. " +
-                    "Please make sure that your credentials file is at the correct " +
-                    "location (~/.aws/credentials), and is in valid format.",
-                    e);
-        }
+        S3Browser browser = new S3Browser( "/home/meteo/.aws/credentials" , "default", "imos-test-data-1" ) ;
+        // S3Browser browser = new S3Browser( s3, bucketName );
 
-        // looking up a single entry... 
-
-        System.out.println("connecting");
-
-        AmazonS3 s3 = new AmazonS3Client(credentials);
-        System.out.println("done connecting");
-        // System.out.flush();
+        recurse( browser, "/home" );
+    }
+}
 
 
-        String bucketName = "imos-test-data-1" ;
-
-        S3Browser browser = new S3Browser( s3, bucketName );
-
-        recurse( browser, "home" );
 
 /*
         // should write a recursive version ... that drills down... 
@@ -129,10 +118,6 @@ public class S3Sample {
         ;
 */
 
-
-
-    }
-}
 
 /*
 
